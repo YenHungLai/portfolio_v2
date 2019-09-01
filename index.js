9cc884b259d4f296329bf188008b30fd8e6834f6
YenHungLai <jacoblai1029@gmail.com>
2019-09-01 16:17:57 -0700

    Added about page content

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const navContacts = document.querySelector('.top-nav a[href="#section-3"]');
 const about = document.querySelector('section[class="about"]');
 const projects = document.querySelector('section[class="projects"]');
 const contacts = document.querySelector('section[class="contacts"]');
+const contactsText = document.querySelector('.contacts div');
+const languageIcons = document.querySelectorAll('.about i[class^="devicon"]');
 
 // Highlight current section
 window.addEventListener('scroll', () => {
-    // Use ratio so it works the same for all viewport size
+	// Use ratio so it works the same for all viewport size
 	const scrollRatio = window.scrollY / container.scrollHeight;
 	console.log(scrollRatio);
 
 	if (scrollRatio >= 0.71) {
 		navContacts.style.color = '#ff3f74';
 		[navProjects, navAbout].forEach(item => (item.style.color = 'white'));
+		// To show animation
+		contactsText.style.display = 'block';
 	} else if (scrollRatio >= 0.45) {
 		navProjects.style.color = '#ff3f74';
 		[navAbout, navContacts].forEach(item => (item.style.color = 'white'));
@@ -24,8 +28,18 @@ window.addEventListener('scroll', () => {
 			item => (item.style.color = 'white')
 		);
 	} else {
-        [navAbout, navProjects, navContacts].forEach(
+		[navAbout, navProjects, navContacts].forEach(
 			item => (item.style.color = 'white')
 		);
-    }
+	}
+});
+
+// Icon hover animation
+languageIcons.forEach(icon => {
+	icon.addEventListener('mouseover', () => {
+		icon.classList.add('animated', 'heartBeat');
+	});
+	icon.addEventListener('mouseout', () => {
+		icon.classList.remove('animated', 'heartBeat');
+	});
 });
