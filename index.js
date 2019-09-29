8c2217a37b840e0e9bd7762b8d35055ce2629363
YenHungLai <jacoblai1029@gmail.com>
2019-09-28 21:35:34 -0700

    Auto hide navbar and added transition

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const projects = document.querySelector('section[class="projects"]');
 const contacts = document.querySelector('section[class="contacts"]');
 const contactsText = document.querySelector('.contacts div');
 const languageIcons = document.querySelectorAll('.about i[class^="devicon"]');
+const topNav = document.querySelector('.top-nav');
+const diagram = document.querySelector('.diagram');
+const gallery = document.querySelector('.gallery');
 
-// Highlight current section
+let isScrolling;
+// Highlight current section and auto hide navbar
 window.addEventListener('scroll', () => {
+	window.clearTimeout(isScrolling);
+	topNav.style.transform = 'translateY(0)';
+
 	// Use ratio so it works the same for all viewport size
 	const scrollRatio = window.scrollY / container.scrollHeight;
-	console.log(scrollRatio);
+	// console.log(scrollRatio);
 
 	if (scrollRatio >= 0.71) {
 		navContacts.style.color = '#ff3f74';
@@ -22,15 +29,32 @@ window.addEventListener('scroll', () => {
 	} else if (scrollRatio >= 0.45) {
 		navProjects.style.color = '#ff3f74';
 		[navAbout, navContacts].forEach(item => (item.style.color = 'white'));
+		gallery.style.opacity = 1;
+		gallery.style.transform = 'translateY(0)';
 	} else if (scrollRatio >= 0.19) {
 		navAbout.style.color = '#ff3f74';
 		[navProjects, navContacts].forEach(
 			item => (item.style.color = 'white')
 		);
+		diagram.style.opacity = 1;
+		diagram.style.transform = 'translateY(0)';
 	} else {
 		[navAbout, navProjects, navContacts].forEach(
 			item => (item.style.color = 'white')
 		);
+		// Reset values for transition
+		diagram.style.opacity = 0;
+		diagram.style.transform = 'translateY(100px)';
+		gallery.style.opacity = 0;
+		gallery.style.transform = 'translateY(100px)';
+		contactsText.style.display = 'none';
+	}
+
+	if (scrollRatio >= 0.24734177215189873) {
+		isScrolling = setTimeout(() => {
+			// console.log('not scrolling');
+			topNav.style.transform = 'translateY(-50px)';
+		}, 2000);
 	}
 });
 
